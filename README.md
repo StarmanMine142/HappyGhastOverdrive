fe84738241ababe5740cf45bc86a89cfd745cb00
makson <maskbatkaev06@gmail.com>
2026-02-20 23:29:26 +0300

    create icon mods

---
+++ README.md
@@ -0,0 +1,12 @@
+Tired of your happy ghast being slow? This mod introduces a new enchantment "Aerial Swiftness."
+
+![Enchanted book](https://cdn.modrinth.com/data/cached_images/a6571853f4bd19b3a7676f4b192c7137ecf57326.png)
+![Enchanted harnesses](https://cdn.modrinth.com/data/cached_images/52de49f5eecfda628090d69909c53bacf5a6f5ea.png)
+___     
+### **Characteristics**
+- Maximum level: 3
+- Maximum speed happy ghast: 15~16 b/s
+___
+### **How to get**
+- In the bastion's treasuries
+- Trading with piglins
--- src/main/java/com/makson/HappyGhastOverdrive.java
+++ src/main/java/com/makson/HappyGhastOverdrive.java
@@ -5,13 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HappyGhastOverdrive implements ModInitializer {
-	public static final String MOD_ID = "happy_ghast_overdrive";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "happy_ghast_overdrive";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
         ModEnchantments.initialize();
         ModItemGroups.register();
         LootModifier.register();
-	}
+    }
 }
